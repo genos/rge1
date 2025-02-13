3d4b5f4b9c05245a60fbe085ed3bdf5fbf6da80f
Graham Enos <graham@strangeworks.com>
2025-02-13 13:33:36 -0500

    squeeze

--- cc/rge1.cc
+++ cc/rge1.cc
@@ -6,11 +6,7 @@ int main() {
   std::uniform_real_distribution<> dist(0.0, 1.0);
   float x = 0;
   for (auto i = 0; i < 1e6; i++) {
-    float total = 0;
-    while (total < 1) {
-      x++;
-      total += dist(rng);
-    }
+    for (float t = 0; t < 1; x++, t += dist(rng)) {}
   }
   x /= 1e6;
   std::cout << x << std::endl;
