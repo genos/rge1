bd8088bcc2ac3667e7d47110e314e622fb0ac59a
Graham <genos@users.noreply.github.com>
2025-02-18 13:40:24 -0500

    ocaml (#8)
    * ocaml
    
    * haskell tweak
    
    * email

--- scheme/rge1.scm
+++ scheme/rge1.scm
@@ -1,12 +1,12 @@
 #!/usr/bin/env chez --optimize-level=3 --script
-(let outer ([i 0] [x 0.0])
-  (if (< i 1000000)
+(let outer ([i 0.0] [x 0.0])
+  (if (< i 1e6)
     (outer (1+ i)
            (+ x
               (let inner ([iters 0.0] [total 0.0])
                 (if (< total 1.0)
                   (inner (1+ iters) (+ total (random 1.0)))
                   iters))))
-    (display (/ x 1000000))))
+    (display (/ x 1e6))))
 (newline)
 (exit)
