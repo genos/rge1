12dd7551ac25e0cb5df5a1b8b0fba89f63c86302
Graham Enos <graham@strangeworks.com>
2025-02-18 15:20:07 -0500

    tighten

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -3,13 +3,13 @@ const std = @import("std");
 pub fn main() !void {
     var rng = std.Random.DefaultPrng.init(1729);
     var x: f64 = 0;
-    for (0..1_000_000) |_| {
+    for (0..1e6) |_| {
         var total: f64 = 0;
         while (total < 1) {
             total += rng.random().float(f64);
             x += 1;
         }
     }
-    x /= 1_000_000;
+    x /= 1e6;
     try std.io.getStdOut().writer().print("{d}\n", .{x});
 }
