b837efdf7df350d225e56ac296d64018359c7931
Graham Enos <graham@strangeworks.com>
2025-02-19 14:00:45 -0500

    generalize merging

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use rand::{rngs::SmallRng, Rng, SeedableRng};
 use rayon::prelude::*;
 
-/// rge1 in rust.
+/// Random ≥ 1 in Rust
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -17,6 +17,11 @@ struct Args {
     seed: u64,
 }
 
+fn merge((n_a, mu_a): (f64, f64), (n_b, mu_b): (f64, f64)) -> (f64, f64) {
+    let n = n_a + n_b;
+    (n, mu_a + (mu_b - mu_a) * n_b / n)
+}
+
 fn main() {
     let args = Args::parse();
     let mut seeds = vec![0u64; args.num_runs];
@@ -26,20 +31,17 @@ fn main() {
         .fold_chunks(
             args.chunk_size,
             || (0.0, 0.0),
-            |(n, mu), seed| {
+            |acc, seed| {
                 let mut rng = SmallRng::seed_from_u64(seed);
                 let (mut x, mut t) = (0.0, 0.0);
                 while t < 1.0 {
                     x += 1.0;
                     t += rng.random::<f64>();
                 }
-                (n + 1.0, mu + (x - mu) / (n + 1.0))
+                merge(acc, (1.0, x))
             },
         )
-        .reduce(
-            || (0.0, 0.0),
-            |(n_a, mu_a), (n_b, mu_b)| (n_a + n_b, mu_a + (mu_b - mu_a) * n_b / (n_a + n_b)),
-        )
+        .reduce(|| (0.0, 0.0), merge)
         .1;
     println!("{x}");
 }
