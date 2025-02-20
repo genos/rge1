14b41024d31f976da9264c42bb06aaf3bfdb1c38
Graham Enos <graham@strangeworks.com>
2025-02-20 13:34:07 -0500

    modernize

--- cc/rge1.cc
+++ cc/rge1.cc
@@ -1,13 +1,22 @@
+#include <algorithm>
 #include <iostream>
 #include <random>
+#include <ranges>
+
+#define N 1000000
 
 int main() {
-  std::mt19937_64 rng(std::random_device{}());
-  std::uniform_real_distribution<> dist(0.0, 1.0);
-  float x = 0;
-  for (auto i = 0; i < 1e6; i++) {
-    for (float t = 0; t < 1; x++, t += dist(rng)) {}
-  }
-  x /= 1e6;
+  std::mt19937_64 r(std::random_device{}());
+  std::uniform_real_distribution<> d(0.0, 1.0);
+  auto i = std::ranges::iota_view{0, N};
+  auto x = std::transform_reduce(
+               std::begin(i), std::end(i), 0.0, std::plus{},
+               [&d, &r](auto _) {
+                 float x = 0;
+                 for (float t = 0; t < 1; x++, t += d(r)) {
+                 }
+                 return x;
+               }) /
+           N;
   std::cout << x << std::endl;
 }
