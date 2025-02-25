9d575dadb9adacbe7ce3c803d0cfbdd9658502d6
Graham Enos <graham@strangeworks.com>
2025-02-25 18:34:24 -0500

    squeeze

--- babashka/rge1.clj
+++ babashka/rge1.clj
@@ -1,6 +1,5 @@
-(let [n 1e6]
-  (println
-    (/ (reduce + (for [_ (range n)]
-                   (loop [i 0.0 t 0.0]
-                     (if (< t 1.0) (recur (inc i) (+ t (rand))) i))))
-       n)))
+(println
+  (/ (reduce + (for [_ (range 1e6)]
+                 (loop [i 0.0 t 0.0]
+                   (if (< t 1.0) (recur (inc i) (+ t (rand))) i))))
+     1e6))
