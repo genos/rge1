244977d65a5f395eab5bb3547620a2b72c68d8ea
Graham Enos <graham@strangeworks.com>
2025-02-28 13:04:34 -0500

    squeeze

--- cc/rge1.cc
+++ cc/rge1.cc
@@ -1,22 +1,17 @@
-#include <algorithm>
 #include <iostream>
 #include <random>
 #include <ranges>
 
-#define N 1000000
-
 int main() {
   std::mt19937_64 r(std::random_device{}());
-  std::uniform_real_distribution<> d(0.0, 1.0);
-  auto i = std::ranges::iota_view{0, N};
-  auto x = std::transform_reduce(
-               std::begin(i), std::end(i), 0.0, std::plus{},
-               [&d, &r](auto _) {
-                 float x = 0;
-                 for (float t = 0; t < 1; x++, t += d(r)) {
-                 }
-                 return x;
-               }) /
-           N;
-  std::cout << x << std::endl;
+  std::uniform_real_distribution d(0.0, 1.0);
+  auto i = std::ranges::iota_view{0, 1'000'000};
+  std::cout << std::transform_reduce(
+      std::begin(i), std::end(i), 0.0, std::plus{},
+      [&d, &r]([[maybe_unused]] auto _) {
+        auto x = 0.0;
+        for (auto t = 0.0; t < 1.0; x++, t += d(r)) {}
+        return x;
+        }) / 1e6
+    << std::endl;
 }
