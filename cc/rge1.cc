a7c961d6d523b1a73d2a5f5621f4bb6b9e2dcce4
Graham Enos <graham@strangeworks.com>
2025-02-28 15:15:05 -0500

    shorten

--- cc/rge1.cc
+++ cc/rge1.cc
@@ -5,7 +5,7 @@
 int main() {
   std::mt19937_64 r(std::random_device{}());
   std::uniform_real_distribution d(0.0, 1.0);
-  auto i = std::ranges::iota_view{0, 1'000'000};
+  auto i = std::views::iota(0, 1'000'000);
   std::cout << std::transform_reduce(
       std::begin(i), std::end(i), 0.0, std::plus{},
       [&d, &r]([[maybe_unused]] auto _) {
