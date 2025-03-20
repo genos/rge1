d5e62db99dec87fc8f31e4e0852423650842e899
Graham Enos <graham@strangeworks.com>
2025-03-20 17:13:42 -0400

    sequential rust version

---
+++ rust-seq/src/main.rs
@@ -0,0 +1,28 @@
+use clap::Parser;
+use rand::{rngs::SmallRng, Rng, SeedableRng};
+
+/// Random ≥ 1 in Rust.
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Number of runs to perform
+    #[arg(long, short, default_value_t = 1_000_000)]
+    num_runs: usize,
+    /// PRNG Seed
+    #[arg(long, short, default_value_t = 1729)]
+    seed: u64,
+}
+
+fn main() {
+    let args = Args::parse();
+    let mut rng = SmallRng::seed_from_u64(args.seed);
+    let mut x = 0.0;
+    for _ in 0..args.num_runs {
+        let mut t = 0.0;
+        while t < 1.0 {
+            x += 1.0;
+            t += rng.random::<f64>();
+        }
+    }
+    println!("{x}");
+}
