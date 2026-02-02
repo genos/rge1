a25a02a53f2cc8e4d9eab43f66a9757e0ff78caf
Graham Enos <graham@strangeworks.com>
2026-02-02 18:36:21 -0500

    unused

--- cc/rge1.cc
+++ cc/rge1.cc
@@ -8,7 +8,7 @@ int main() {
   auto i = std::ranges::iota_view(0, 1'000'000);
   std::cout << std::transform_reduce(
       std::begin(i), std::end(i), 0.0, std::plus{},
-      [](auto _) {
+      []([[maybe_unused]] auto _) {
         auto x = 0.0;
         for (auto t = 0.0; t < 1.0; x++, t += ldexp(pcg32_random(), -32)) {}
         return x;
