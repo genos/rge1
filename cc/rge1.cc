95f8632e836732f54f987e17c22b3ddd54774ae8
Graham Enos <graham@strangeworks.com>
2026-02-02 16:39:18 -0500

    c/c++ stuff

--- cc/rge1.cc
+++ cc/rge1.cc
@@ -5,13 +5,12 @@
 
 int main() {
   pcg32_srandom(42u, 54u);
-  auto i = std::views::iota(0, 1'000'000);
+  auto i = std::ranges::iota_view(0, 1'000'000);
   std::cout << std::transform_reduce(
       std::begin(i), std::end(i), 0.0, std::plus{},
-      []([[maybe_unused]] auto _) {
+      [](auto _) {
         auto x = 0.0;
         for (auto t = 0.0; t < 1.0; x++, t += ldexp(pcg32_random(), -32)) {}
         return x;
-        }) / 1e6
-    << std::endl;
+      }) / 1e6 << std::endl;
 }
